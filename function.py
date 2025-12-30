0634cc971c5923666a9aa45ce6260673fad4dbda
Sasmit Pudasaini <162982043+sasmitpudasaini@users.noreply.github.com>
2025-12-30 13:59:04 +0545

    high order function

--- function.py
+++ function.py
@@ -1,67 +1,67 @@
 
-#some built in function:
-for i in range(1,50):
-    print(i)
+# #some built in function:
+# for i in range(1,50):
+#     print(i)
 
-a = [1,2,3,4]
-print(len(a))
+# a = [1,2,3,4]
+# print(len(a))
 
-print(id(a))
+# print(id(a))
 
-#greeting with function
+# #greeting with function
 
-def greet_student(name):
-    print(f'hello, {name}!welcome to python class')
-    return f"Good day, {name}!"
-name = "Alice"
-result = greet_student(name)
-print(result)
+# def greet_student(name):
+#     print(f'hello, {name}!welcome to python class')
+#     return f"Good day, {name}!"
+# name = "Alice"
+# result = greet_student(name)
+# print(result)
 
 
-def greet_student(name,grade):
-    print (f"my name is {name}, {grade}")
-    return f"Good day, {name}!{grade}"
-print(greet_student('sasmit',5))
+# def greet_student(name,grade):
+#     print (f"my name is {name}, {grade}")
+#     return f"Good day, {name}!{grade}"
+# print(greet_student('sasmit',5))
 
-#calculate the vo;ume of the cuboid
-def volume_cuboid(length=1,breadth=2,height=3):
-    return f"the volume of the cuboid is {length*breadth*height }"
+# #calculate the vo;ume of the cuboid
+# def volume_cuboid(length=1,breadth=2,height=3):
+#     return f"the volume of the cuboid is {length*breadth*height }"
     
-print(volume_cuboid())
+# print(volume_cuboid())
 
-#print reverse using function
-def reverse_value(value):
-    return f"the volume of the cuboid is {value[::-1] }"
+# #print reverse using function
+# def reverse_value(value):
+#     return f"the volume of the cuboid is {value[::-1] }"
 
-print(reverse_value(value = [1,2,3]))
+# print(reverse_value(value = [1,2,3]))
 
-#print a name multiple time
+# #print a name multiple time
 
-def reverse_value():
-    z = (5)
-    k = "sasmit"
-    for i in range(1,z+1):
-        print(k)
+# def reverse_value():
+#     z = (5)
+#     k = "sasmit"
+#     for i in range(1,z+1):
+#         print(k)
 
-print(reverse_value())
+# print(reverse_value())
 
-#recursive function
-def fibonacci(n):
-    if n<=1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+# #recursive function
+# def fibonacci(n):
+#     if n<=1:
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
 
-print (fibonacci(5))
+# print (fibonacci(5))
 
-#sum of natural numbers using recursive function
-def sum_naturalnum(n):
-    if n==1:
-        return n 
-    else:
-        return n +sum_naturalnum(n-1)
+# #sum of natural numbers using recursive function
+# def sum_naturalnum(n):
+#     if n==1:
+#         return n 
+#     else:
+#         return n +sum_naturalnum(n-1)
     
-print (sum_naturalnum(5))
+# print (sum_naturalnum(5))
 
 #from the list find the smallest number using the recursive function
 
@@ -75,28 +75,72 @@ print (sum_naturalnum(5))
             
 #Wap to find palindrome using recursion
 
-def pali(n):
-    if n==n[::-1]:
-        return "it is palindrome"
-    else:
-        return "it is not palindrome"
+# def pali(n):
+#     if n==n[::-1]:
+#         return "it is palindrome"
+#     else:
+#         return "it is not palindrome"
     
-print(pali('1232'))
+# print(pali('1232'))
             
-#find leap year
+# #find leap year
 
-def leap_year(date):
-    if (date % 4 == 0 and date % 100 != 0) or (date % 400 == 0):
-        return ("the next leap year is ",date)
-    else:
-        return leap_year(date+1)
-print(leap_year(4))
+# def leap_year(date):
+#     if (date % 4 == 0 and date % 100 != 0) or (date % 400 == 0):
+#         return ("the next leap year is ",date)
+#     else:
+#         return leap_year(date+1)
+# print(leap_year(4))
 
 
-def lala(data):
-    if data == 1 :
-        return True
-    else:
-        return False
-print(lala(1))
+# def lala(data):
+#     if data == 1 :
+#         return True
+#     else:
+#         return False
+# print(lala(1))
+
+#high order functions
+
+# def add(x,y):
+#     return(x+y)
+
+# def subtract(x,y):
+#     return(x+y)
+
+# def multilpy(x,y):
+#     return(x+y)
+# def calculator(func,x,y):
+#     return func(x,y)
+# print(calculator((10,2)))
+
+def highfunc(func):
+    def insidefunc(*args, **kwargs):
+        print(f"function{func.__name__} is called with arguments {args} and {kwargs}")
+        result = func(*args, **kwargs)
+        print (f"function {func.__name__} returned {result}")
+        return result
+    return insidefunc
+
+@highfunc
+def rev(s):
+    return s[::-1]
+s = 'sasmit'
+print(rev(s))
+
+def multiplier(factor):
+    def multiply_by_factor(number):
+        return number * factor
+    return multiply_by_factor
+
+multiplier_value = multiplier(5)
+print(multiplier_value.__name__)
+print(type(multiplier_value))
+result = multiplier_value(10)
+print(result)
+
+
+
+
+
 
